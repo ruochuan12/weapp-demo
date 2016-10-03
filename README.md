fd1a7e8c2074a64485e33839a86954361b93d242
lxchuan12 <lxchuan12@163.com>
2016-10-03 19:10:27 +0800

    second commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# weapp-demo
+# 微信小程序的demo
+## 官方文档
+
+- [小程序开发文档](https://mp.weixin.qq.com/debug/wxadoc/dev/index.html)
+- [小程序设计指南](https://mp.weixin.qq.com/debug/wxadoc/design/index.html)
+- [小程序开发者工具](https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/download.html)
+
+## 其他资料
+
+[微信小程序开发资源汇总 wechat weapp](https://github.com/justjavac/awesome-wechat-weapp)
+
--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,8 @@ Page({
     userInfo: {},
     btnText:'showHello',
     showHello:true,
-    items:[1,2,3]
+    items:[1,2,3],
+    imageUrl:null
   },
   //事件处理函数
   bindViewTap: function() {
@@ -19,6 +20,20 @@ Page({
     console.log('按钮被点击了');
     this.setData({
       showHello:!this.data.showHello
+    });
+  },
+  chooseImage:function(){
+    var page = this;
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths;
+        console.log(tempFilePaths);
+        page.setData({imageUrl:tempFilePaths});
+      }
     });
   },
   onLoad: function () {
